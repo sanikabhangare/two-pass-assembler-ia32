d587ad6fca53cd5655062d9ad1770605017244ef
Kishna G Agrawal <agrawalkrishna9904@gmail.com>
2026-08-25 12:11:59 +0530

    Add sample assembly instructions for validation.

---
+++ text.asm
@@ -0,0 +1,15 @@
+
+        mov epx,ebx
+        mov edx,ecx
+        add eax,ecx
+        sub ebx,edx
+        cmp ebp,esp
+        inc eax,ebx
+        dec edx
+        inc esi
+        inc esp
+        dec ebx
+        dec edi
+        xor ecx,ecx
+        and eax,ebx
+        qr esi,ebx
